5e75050205575fcc7377a935565a2a9b1b10689b
Cristian Cuevas <Ccuevas3410@gmail.com>
2020-06-03 20:21:03 -0700

    Adding message in white bar

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var squares = document.querySelectorAll('.square');
 var spanRgb = document.querySelector("#rgb");
+var messageDisplay = document.getElementById("message");
 
-var colors =[]
+var colors = []
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
 function randomColor() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -16,22 +17,11 @@ function randomColor() {
 
 
 for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = randomColor();
-    colors.push(squares[i].style.backgroundColor);
-
-}
-
-
-for (var i = 0; i < squares.length; i++) {
 
     //Adding initial colors to squares
     squares[i].style.backgroundColor = randomColor();
-
     //Using a list to keep track of RGB colors
     colors.push(squares[i].style.backgroundColor);
-
-
-    
 
 
 }
@@ -41,3 +31,22 @@ pickedColor = colors[getRandomInt(5)];
 spanRgb.textContent = pickedColor;
 
 
+//Adding event listener to each square to get the message of right or wrong color.
+for (var j = 0; j < squares.length; j++) {
+
+
+    squares[j].addEventListener("click", function () {
+
+        clickedColor = this.style.backgroundColor;
+
+        if (clickedColor === pickedColor){
+            messageDisplay.textContent = "Correct!";
+
+        } else {
+            messageDisplay.textContent = "Wrong, try again!";
+            this.style.backgroundColor = "black";
+        }
+    });
+
+
+}
